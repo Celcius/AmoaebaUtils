449fdc7c6d0b281adbeb20051808040f0211d957
zoreander <ivo.capelo@gmail.com>
2020-09-26 17:32:08 +0100

    IndexableStack

---
+++ AmoaebaUtils/Core/IndexableStack.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AmoaebaUtils
+{
+public class IndexableStack<T> : List<T>
+{
+
+    public void Enqueue(T value)
+    {
+        Add(value);
+    }
+
+    public void Dequeue()
+    {
+        if(Count == 0)
+        {
+            return;
+        }
+        RemoveAt(Count-1);
+    }
+
+    public T Peek()
+    {
+        if(Count == 0)
+        {
+            throw new System.Exception("Peeking at empty queue");
+        }
+
+        return this[Count-1];
+    }
+}
+}
