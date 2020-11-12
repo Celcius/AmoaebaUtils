39562acd963c959c68c932b7f190706ec00b1161
zoreander <ivo.capelo@gmail.com>
2020-11-12 21:48:19 +0000

    FloatVarToSlider and accessible Setup
    (cherry picked from commit 3fb2dd1249ca5df0a444abe093c91010789c0784)

---
+++ AmoaebaUtils/MenuUtils/FloatVarToSlider.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using AmoaebaUtils;
+
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditor.UI;
+#endif
+
+namespace AmoaebaUtils
+{
+[System.Serializable]
+public class FloatVarToSlider : Slider
+{
+
+    public bool useRatio = false;
+
+    [SerializeField]
+    private FloatVar slideVar;
+    public FloatVar SliderVar
+    {
+        get { return slideVar; }    
+        set 
+        { 
+            if(slideVar != null)
+            {
+                slideVar.OnChange -= OnVarChanged;
+            }
+            slideVar = value; 
+        }
+    }
+    
+    [SerializeField]
+    private bool storeValue = true;
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+        if(slideVar != null)
+        {
+            slideVar.OnChange += OnVarChanged;
+            OnVarChanged(0, slideVar.Value);
+        }
+        onValueChanged.AddListener(OnValueChanged);
+    }
+
+    protected void OnVarChanged(float oldVal, float newVal)
+    {
+        this.value = GetValue(newVal);
+    }
+
+    protected override void OnDestroy()
+    {
+        if(slideVar != null)
+        {
+            slideVar.OnChange -= OnVarChanged;
+        }
+
+        if(storeValue)
+        {
+            slideVar.SetupValue = slideVar.Value;
+        }
+        
+        onValueChanged.RemoveListener(OnValueChanged);
+        base.OnDestroy();
+    }
+
+    private void OnValueChanged(float val)
+    {
+        if(slideVar != null)
+        {
+            slideVar.Value = GetRatio(val); 
+            if(storeValue)
+            {
+                slideVar.SetupValue = slideVar.Value;
+            }      
+        }
+    }
+
+    private float GetRatio(float val)
+    {
+        return useRatio?  (val - minValue) / (maxValue - minValue) : val;
+    }
+
+    private float GetValue(float ratio)
+    {
+        return useRatio? (ratio * (maxValue - minValue) + minValue) : ratio;
+    }
+}
+
+
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(FloatVarToSlider))]
+public class FloatVarToSliderEditor : SliderEditor 
+{
+        FloatVarToSlider sliderTarget;
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+            sliderTarget = (FloatVarToSlider)target;
+        }
+
+        public override void OnInspectorGUI ()
+        {
+           base.OnInspectorGUI();
+          
+           EditorGUILayout.Space();
+
+           FloatVar var = EditorGUILayout.ObjectField("Slider Variable", sliderTarget.SliderVar, typeof(FloatVar), false) as FloatVar;
+           sliderTarget.useRatio = EditorGUILayout.Toggle("Use Ratio", sliderTarget.useRatio);
+            
+           if(var != sliderTarget.SliderVar)
+           {
+               sliderTarget.SliderVar = var;
+               EditorUtility.SetDirty(sliderTarget);  
+
+           }
+        }
+}
+#endif 
+}
