c005fbb24f42afb3a6919bc1dec99d8391e98c04
zoreander <ivo.capelo@gmail.com>
2020-11-19 18:29:06 +0000

    Added Menu Utils
    (cherry picked from commit db2e7493e814e81c30c6728691954575c108ecba)

---
+++ AmoaebaUtils/Core/DisableOnBoolVar.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AmoaebaUtils
+{
+public class DisableOnBoolVar : MonoBehaviour
+{
+    [SerializeField]
+    private BoolVar boolVar;
+
+    [SerializeField]
+    private bool valueToDisable = false;
+
+    [SerializeField]
+    private bool OnlyCheckStart = true;
+
+    private void Start()
+    {
+        if(!OnlyCheckStart)
+        {
+            boolVar.OnChange += OnValueChange;
+        }
+        
+        OnValueChange(false, boolVar.Value);
+    }
+
+    private void OnDestroy() 
+    {
+        boolVar.OnChange -= OnValueChange;
+    }
+
+    private void OnValueChange(bool oldVal, bool newVal)
+    {
+        gameObject.SetActive(!(newVal == valueToDisable));
+    }
+}
+}
---
+++ AmoaebaUtils/MenuUtils/FloatVarToFillImage.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace AmoaebaUtils
+{
+public class FloatVarToFillImage : MonoBehaviour
+{
+    [SerializeField]
+    private FloatVar fillVar;
+
+    [SerializeField]
+    private Image fillImage;
+
+    private void Start()
+    {
+        fillVar.OnChange += OnValueChange;
+        OnValueChange(0, fillVar.Value);
+    }
+
+    private void OnDestroy() 
+    {
+        fillVar.OnChange -= OnValueChange;
+    }
+
+    private void OnValueChange(float oldVal, float newVal)
+    {
+        fillImage.fillAmount = Mathf.Clamp(newVal, 0, 1.0f);
+    }
+}
+}
