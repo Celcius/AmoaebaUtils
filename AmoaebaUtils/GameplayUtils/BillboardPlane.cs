3f791858254a202f51e79b234378d1f86a0bbae6
zoreander <ivo.capelo@gmail.com>
2020-11-28 22:04:51 +0000

    AddedBillboardPlane
    (cherry picked from commit 27d65925584cedf1b88276f2773c4f02f9a76426)

---
+++ AmoaebaUtils/GameplayUtils/BillboardPlane.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AmoaebaUtils
+{
+public class BillboardPlane : MonoBehaviour
+{
+    [SerializeField]
+    private Transform cameraTransform;
+
+    void Update()
+    {
+        if(cameraTransform == null)
+        {
+            return;
+        }
+
+        transform.forward = transform.position - cameraTransform.position;
+    }
+}
+}
