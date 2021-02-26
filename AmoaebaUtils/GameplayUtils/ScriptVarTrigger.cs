37b7b39d026be15bf3e352d9c15252f8a4965234
zoreander <ivo.capelo@gmail.com>
2021-02-26 20:24:52 +0000

    ScriptVar Triggers

---
+++ AmoaebaUtils/GameplayUtils/ScriptVarTrigger.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AmoaebaUtils
+{
+public class ScriptVarTrigger<T,V> : MonoBehaviour where V : ScriptVar<T>
+{
+    [SerializeField]
+    private V var;
+
+    [SerializeField]
+    private T valueOnCollision;
+
+    [SerializeField]
+    private bool shootOnce = true;
+    private bool hasShot;
+
+    private void OnTriggerEnter(Collider other) 
+    {
+        OnTrigger();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        OnTrigger();
+    }
+
+    public virtual void OnTrigger()
+    {
+        if(hasShot && shootOnce)
+        {
+            return;
+        }
+        var.Value = valueOnCollision;
+        hasShot = true;
+    }
+}
+}
