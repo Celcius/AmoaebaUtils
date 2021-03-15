8dea6f71b49321639c707e9b0f1af6421a2d83f9
zoreander <ivo.capelo@gmail.com>
2021-03-15 07:50:35 +0000

    Darkness Shader

---
+++ AmoaebaUtils/GameplayUtils/Shaders/DarknessShader.shader
@@ -0,0 +1,93 @@
+﻿// Upgrade NOTE: replaced 'mul(UNITY_MATRIX_MVP,*)' with 'UnityObjectToClipPos(*)'
+
+Shader "AmoaebaUtils/DarknessShader"
+{
+    Properties
+    {
+        _outerColor("Outer Color", Color) = (0,0.5843137254901961,1,1)
+        _innerColor("Inner Color", Color) = (0.1176470588235294,0,0.5882352941176471,1)
+        _innerRadius("Inner Radius", Range(0, 1)) = 0.5
+        _border("Fade Border", Range(0.00001, 1)) = 0.01
+        _xOffset("XOffset", Range(-1,2)) = 0.5
+        _yOffset("YOffset", Range(-1,2)) = 0.5
+        _opacity("Opacity", Range(0,1)) = 1
+    }
+        SubShader
+    {
+        Tags { "RenderType" = "Transparent" }
+        LOD 100
+
+        Pass
+        {
+            ZWrite Off // don't write to depth buffer 
+            // in order not to occlude other objects
+
+            Blend SrcAlpha OneMinusSrcAlpha // use alpha blending
+
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            // make fog work
+            #pragma multi_compile_fog
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            float _border;
+
+            float4 _outerColor;
+            float4 _innerColor;
+            float _innerRadius;
+            float _xOffset;
+            float _yOffset;
+            float _opacity;
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+            };
+
+            v2f vert(
+                float4 vertex : POSITION, // vertex position input
+                float2 uv : TEXCOORD0, // texture coordinate input
+                out float4 outpos : SV_POSITION // clip space position output
+            )
+            {
+                v2f o;
+                o.uv = uv;
+                outpos = UnityObjectToClipPos(vertex);
+                return o;
+            }
+
+            float2 antialias(float radius, float borderSize, float dist)
+            {
+                float t = smoothstep(radius + borderSize, radius - borderSize, dist);
+                return t;
+            }
+
+            fixed4 frag(v2f i, UNITY_VPOS_TYPE screenPos : VPOS) : SV_Target
+            {
+                float4 col;
+                float2 center = float2((_xOffset)*_ScreenParams.x,(_yOffset)*_ScreenParams.y); //_ScreenParams.xy / 2;
+
+                float maxradius = length(_ScreenParams.xy / 2);
+
+                float radius = maxradius*(_innerRadius);
+
+                float dis = distance(screenPos.xy, center);
+
+                float aliasVal = antialias(radius, _border*1000, dis);
+                col = lerp(_outerColor, _innerColor, aliasVal);
+                col.a = col.a * _opacity;
+                return col;
+
+            }
+            ENDCG
+        }
+    }
+}
---
+++ AmoaebaUtils/GameplayUtils/Shaders/ShaderInstanceUtils.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AmoaebaUtils
+{
+public static class ShaderInstanceUtils
+{
+    public const string darknessOuterColor = "_outerColor";
+    public const string darknessInnerColor = "_innerColor";
+    public const string darkessRadius = "_innerRadius";
+    public const string darknessBorder = "_border";
+    public const string darknessXOffset = "_xOffset";
+    public const string darknessYOffset = "_yOffset";
+    public const string darknessOpacity = "_opacity";
+
+
+    public static Color GetDarknessInnerColor(Material material)
+    {
+        const string colorStr = darknessInnerColor;
+        return GetColor(material, colorStr);
+    }
+
+    public static void SetDarknessInnerColor(Color color, Material material)
+    {
+        const string colorStr = darknessInnerColor;
+        material.SetColor(colorStr, color);
+    }
+
+    public static Color GetDarknessOuterColor(Material material)
+    {
+        const string colorStr = darknessOuterColor;
+        return GetColor(material, colorStr);
+    }
+
+    public static void GetDarknessOuterColor(Color color, Material material)
+    {
+        const string colorStr = darknessOuterColor;
+        material.SetColor(colorStr, color);
+    }
+
+    public static void SetDarknessMaterialPosition(float xPos, float yPos, Material material)
+    {   
+        const string posXStr = darknessXOffset;
+        const string posYStr = darknessYOffset;
+        
+        material.SetFloat(posXStr, xPos);
+        material.SetFloat(posYStr, yPos);
+    }
+
+    public static float GetDarknessPosX(Material material)
+    {
+        return GetFloat(material, darknessXOffset);
+    }
+
+    public static float GetDarknessPosY(Material material)
+    {
+        return GetFloat(material, darknessYOffset);
+    }
+
+    public static float GetDarknessRadius(Material material)
+    {
+        return GetFloat(material, darkessRadius);
+    }
+
+    public static float GetDarknessBorder(Material material)
+    {
+        return GetFloat(material, darknessBorder);
+    }
+
+    public static float GetDarknessOpacity(Material material)
+    {
+        return GetFloat(material, darknessOpacity);
+    }
+
+    public static void SetDarknessPosX(Material material, float val)
+    {
+        SetFloat(material, darknessXOffset, val);
+    }
+
+    public static void SetDarknessPosY(Material material, float val)
+    {
+        SetFloat(material, darknessYOffset, val);
+    }
+
+    public static void SetDarknessRadius(Material material, float val)
+    {
+        SetFloat(material, darkessRadius, val);
+    }
+
+    public static void SetDarknessBorder(Material material, float val)
+    {
+        SetFloat(material, darknessBorder, val);
+    }
+
+    public static void SetDarknessOpacity(Material material, float val)
+    {
+        SetFloat(material, darknessOpacity, val);
+    }
+
+
+    private static Color GetColor(Material material, string param)
+    {
+        return material.GetColor(param);
+    }
+
+    private static void SetColor(Material material, string param, Color color)
+    {
+        material.SetColor(param,color);
+    }
+
+    private static float GetFloat(Material material, string param)
+    {
+        return material.GetFloat(param);
+    }
+
+    private static void SetFloat(Material material, string param, float val)
+    {
+        material.SetFloat(param, val);
+    }
+        
+}
+}
